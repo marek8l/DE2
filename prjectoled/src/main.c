f83daf5b17c6fc9f82ae9fe28a4abe0b35896e56
marek8l <marekivan7@gmail.com>
2023-11-29 16:36:48 +0100

    projektOLED

---
+++ prjectoled/src/main.c
@@ -0,0 +1,33 @@
+// https://github.com/Sylaina/oled-display
+
+#include <oled.h>
+
+int main(void)
+{
+    oled_init(OLED_DISP_ON);
+    oled_clrscr();
+
+    oled_charMode(DOUBLESIZE);
+    oled_puts("You ");
+
+    oled_charMode(NORMALSIZE);
+
+    // oled_gotoxy(x, y)
+    oled_gotoxy(0, 2);
+    oled_puts("Is");
+
+    // oled_drawLine(x1, y1, x2, y2, color)
+    oled_drawLine(0, 25, 120, 25, WHITE);
+
+    oled_gotoxy(0, 4);
+    oled_puts("Nigger");
+
+    // Copy buffer to display RAM
+    oled_display();
+
+    while (1) {
+        ;
+    }
+
+    return 0;
+}
